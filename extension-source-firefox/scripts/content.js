cb750eba40a9950aa09298bc0d73dd971ab7f6a4
Stefan Bohacek <stefan@fourtonfish.com>
2022-09-23 12:24:02 -0400

    Updated getMeta function to work with latest changes to GitHub's organization page HTML changes.

--- extension-source-firefox/scripts/content.js
+++ extension-source-firefox/scripts/content.js
@@ -37,7 +37,7 @@ ready( function(){
     const metas = document.getElementsByTagName('meta');
 
     for ( let i = 0; i < metas.length; i++ ) {
-      if ( metas[i].getAttribute( 'property' ) === metaName ) {
+      if ( metas[i].getAttribute( 'name' ) === metaName ) {
         return metas[i].getAttribute( 'content' );
       }
     }
