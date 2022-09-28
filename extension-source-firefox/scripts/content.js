667e3e8746a6ec4155469294835ccc2913d10f51
Stefan Bohacek <stefan@fourtonfish.com>
2022-09-28 01:30:40 -0400

    Added a link to issues tagged with "help wanted". Minor code refactor.

--- extension-source-firefox/scripts/content.js
+++ extension-source-firefox/scripts/content.js
@@ -1,4 +1,4 @@
-function ready( fn ) {
+const ready = ( fn ) => {
   if ( document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading' ){
     fn();
   } else {
@@ -6,8 +6,34 @@ function ready( fn ) {
   }
 }
 
-ready( function(){
-  function formatNumber(number, decimals, dec_point, thousands_sep) {
+ready( () => {
+  const addLink = ( options ) => {
+    let linkWrapper = document.createElement( 'li' );
+    linkWrapper.classList.add( 'd-flex' );
+    linkWrapper.classList.add( 'js-responsive-underlinenav-item' );
+
+    let link = document.createElement( 'a' );
+
+    link.setAttribute( 'href', options.linkURL );
+    link.innerHTML = options.linkHTML;
+    link.classList.add( 'UnderlineNav-item' );
+    linkWrapper.appendChild( link );
+    orgnav.querySelector('ul').appendChild( linkWrapper );
+
+    if (options.dataURL){
+      fetch( options.dataURL )
+      .then( response => response.json() )
+      .then( data => {
+        if ( data && data[options.dataValue] ){
+          document.getElementById( options.dataID ).innerHTML = formatNumber( data[options.dataValue] );
+        }
+      } ).catch( ( err ) => {
+        console.log( err.message );
+      } );
+    }
+  }
+
+  const formatNumber = (number, decimals, dec_point, thousands_sep) => {
     /* Source:
        https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript/2901136#2901136
     */
@@ -15,7 +41,7 @@ ready( function(){
           prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
           sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
           dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-          toFixedFix = function (n, prec) {
+          toFixedFix = (n, prec) => {
               // Fix for IE parseFloat(0.55).toFixed(0) = 0;
               var k = Math.pow(10, prec);
               return Math.round(n * k) / k;
@@ -31,13 +57,13 @@ ready( function(){
       return s.join(dec);
   }
 
-  function getMeta( metaName ) {
+  const getMeta = ( metaName ) => {
     /* Source:
        https://stackoverflow.com/questions/7524585/how-do-i-get-the-information-from-a-meta-tag-with-javascript/7524621#7524621 */
     const metas = document.getElementsByTagName('meta');
 
     for ( let i = 0; i < metas.length; i++ ) {
-      if ( metas[i].getAttribute( 'name' ) === metaName ) {
+      if ( metas[i].getAttribute( 'property' ) === metaName || metas[i].getAttribute( 'name' ) === metaName ) {
         return metas[i].getAttribute( 'content' );
       }
     }
@@ -51,29 +77,17 @@ ready( function(){
     const orgName = getMeta( 'profile:username' );
 
     if ( orgName ){
-      const apiCallURL = `https://api.github.com/search/issues?q=org:${ orgName }+state:open&per_page=100&sort=created&order=asc`
-
-      let issuesLinkWrapper = document.createElement( 'li' );
-      issuesLinkWrapper.classList.add( 'd-flex' );
-      issuesLinkWrapper.classList.add( 'js-responsive-underlinenav-item' );
-
-      let issuesLink = document.createElement( 'a' );
-
-      issuesLink.setAttribute( 'href', `https://github.com/search?q=org%3A${ orgName }+is%3Aissue&state=open&type=Issues` );
-      issuesLink.innerHTML = 'Issues <span id="org-issues-counter" class="Counter"></span>';
-      issuesLink.classList.add( 'UnderlineNav-item' );
-      issuesLinkWrapper.appendChild( issuesLink );
-      orgnav.querySelector('ul').appendChild( issuesLinkWrapper );
-
-      fetch( apiCallURL )
-        .then( response => response.json() )
-        .then( data => {
-          if ( data && data.total_count ){
-            document.getElementById( 'org-issues-counter' ).innerHTML = formatNumber( data.total_count );
-          }
-        } ).catch( function( err ) {
-          console.log( err.message );
-        } );
+      addLink({
+        linkURL: `https://github.com/search?q=org%3A${ orgName }+is%3Aissue&state=open&type=Issues`,
+        linkHTML: `Issues <span id="org-issues-counter" class="Counter"></span>`,
+        dataURL: `https://api.github.com/search/issues?q=org:${ orgName }+state:open&per_page=100&sort=created&order=asc`,
+        dataID: 'org-issues-counter',
+        dataValue: 'total_count'
+      });
+      addLink({
+        linkURL: `https://github.com/search?q=user%3A${ orgName }+label%3A%22help+wanted%22&type=Issues&ref=advsearch`,
+        linkHTML: `Help wanted`
+      });
     }
   }
 } );
